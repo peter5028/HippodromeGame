d2d8c4fa85647a6d8765be2da8a712532401d54d
peter5028 <59503024+peter5028@users.noreply.github.com>
2021-01-11 22:15:40 +0000

    Initial commit

---
+++ Hippodrome.java
@@ -0,0 +1,62 @@
+package com.codegym.task.task21.task2113;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Hippodrome {
+
+    static Hippodrome game;
+
+    private List<Horse> horses;
+
+    void run() throws InterruptedException {
+        for (int i = 0; i < 100; i++) {
+            move();
+            print();
+            Thread.sleep(200);
+        }
+
+    }
+    void move() {
+        for (Horse horse : horses) {
+            horse.move();
+        }
+    }
+    void print() {
+        for (Horse horse : horses) {
+            horse.print();
+        }
+        for (int i = 0; i < 10; i++) {
+            System.out.println();
+        }
+    }
+    public Horse getWinner() {
+        Horse result = horses.get(0);
+        for (Horse horse : horses) {
+            if (horse.getDistance() > result.getDistance())
+                result = horse;
+        }
+        return result;
+    }
+
+    public void printWinner() {
+        System.out.println("The winner is " + getWinner().name + "!");
+    }
+
+    public Hippodrome (List<Horse> horses) {
+        this.horses = horses;
+    }
+
+    public List<Horse> getHorses() {
+        return horses;
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        game = new Hippodrome(new ArrayList<>());
+        game.getHorses().add(new Horse("Lucky 7", 3, 0));
+        game.getHorses().add(new Horse("Black Jack", 3, 0));
+        game.getHorses().add(new Horse("Silver Arrow", 3, 0));
+        game.run();
+        game.printWinner();
+    }
+}
